19f55330db1dc8cb855bd7d5857f1c95f7aa1ffb
Daniel <71315420+MonteDan@users.noreply.github.com>
2022-07-18 23:40:20 +0200

    verticalni slider
    Nemám už dneska buňky na tu reactovou věc (react-spring), plain css horizontální slider je nad my schopnosti

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import ogimage from "astro-og-image";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-    ogimage({
-      config: {
-        path: "/posts/",
-      },
-    }),
-  ],
+  integrations: [tailwind(), svelte(), ogimage({
+    config: {
+      path: "/posts/"
+    }
+  }), react()]
 });
