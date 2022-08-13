7204cec1be9c35f34da59d1cf3cb5e47d1fa4790
Tomáš Kebrle <tech@tomaskebrle.cz>
2022-08-13 23:11:41 +0200

    Removed unnecassary dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import ogimage from "astro-og-image";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind()],
 });
